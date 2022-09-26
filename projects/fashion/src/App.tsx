23be092451e1a9006263024ac97e1df982ffd8cd
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-09-27 00:49:11 +0600

    implemented hero section

--- projects/fashion/src/App.tsx
+++ projects/fashion/src/App.tsx
@@ -1,5 +1,8 @@
-import { ReactComponent as Logo } from "./assets/logo.svg";
 import clsx from "clsx";
+
+import { ReactComponent as BlobIcon } from "./assets/blob.svg";
+import heroImage from "./assets/hero-image.png";
+import { ReactComponent as Logo } from "./assets/logo.svg";
 
 const links = [
   {
@@ -38,6 +41,7 @@ function App() {
         <div className="flex gap-4 items-center">
           {links.map((link) => (
             <a
+              key={link.label}
               href={link.href}
               {...link.props}
               className={clsx(
@@ -50,6 +54,39 @@ function App() {
           ))}
         </div>
       </header>
+
+      <section
+        id="hero"
+        className="mx-16 my-8 rounded-3xl bg-hero bg-cover p-20 flex justify-between relative"
+      >
+        <section>
+          <h1 className="uppercase text-6xl font-black leading-snug break-words max-w-title relative isolate">
+            <span className="absolute top-0 -left-6 w-full h-mark bg-white -z-10 -rotate-2" />
+            <span className="absolute top-1/2 -left-6 w-full h-mark bg-primary -z-10 -rotate-2" />
+            LET’S EXPLORE UNIQUE CLOTHES.
+          </h1>
+
+          <p className="tracking-tight text-xl mb-0 mt-6">
+            Live for Influential and Innovative fashion!
+          </p>
+
+          <section className="flex items-center gap-4 mt-10">
+            <BlobIcon className="fill-primary" />
+            <button
+              type="button"
+              className="text-white font-medium bg-black px-5 py-3 rounded-md"
+            >
+              Shop Now
+            </button>
+          </section>
+        </section>
+
+        <img
+          src={heroImage}
+          alt="Hero"
+          className="absolute bottom-0 right-2 h-5/6 my-0"
+        />
+      </section>
     </main>
   );
 }
--- projects/fashion/tailwind.config.cjs
+++ projects/fashion/tailwind.config.cjs
@@ -5,8 +5,20 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,json}", "./index.html"],
   theme: {
     extend: {
+      colors: {
+        primary: "#EBD96B",
+      },
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        hero: "url('/hero-bg.svg')",
+      },
+      maxWidth: {
+        title: "8ch",
+      },
+      height: {
+        mark: "1.33em",
       },
     },
   },
