c7f68252fc5d9cdf6d02725d4df9ec2465a85f69
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-09-27 16:17:49 +0600

    added api token in urql headers

--- apps/web/src/services/urql-client.ts
+++ apps/web/src/services/urql-client.ts
@@ -7,6 +7,12 @@ export const ssr = ssrExchange({ isClient: typeof window !== "undefined" });
 export function createUrqlClient(ssrExchange = ssr) {
   return createClient({
     url: process.env.NEXT_PUBLIC_API_URL!,
+    fetchOptions: {
+      headers: {
+        // eslint-disable-next-line
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+      },
+    },
     exchanges: [dedupExchange, cache, fetchExchange, ssrExchange],
   });
 }
